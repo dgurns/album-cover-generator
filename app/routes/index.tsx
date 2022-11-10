c641847084eec61dd9c2981e874047d15a0f0ff2
Dan Gurney <gurney.dan@gmail.com>
2022-11-10 11:35:58 +0000

    feat: set up prettier

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,16 +1,26 @@
+import { Form } from '@remix-run/react';
+
 export default function Home() {
 	return (
-		<div className="flex flex-col items-center justify-start w-full h-screen">
-			<div className="flex w-full md:max-w-md h-screen md:h-auto flex-col items-center justify-start py-4 px-4">
-				<div className="flex flex-row items-center justify-start mt-4">
-					Title
+		<div className="flex h-screen w-full flex-col items-center justify-start">
+			<div className="flex h-screen w-full flex-col items-center justify-start py-4 px-4 md:h-auto md:max-w-md">
+				<div className="mt-4 flex flex-col items-center justify-start text-center">
+					<h1>Album Cover Generator</h1>
+					<div className="text-gray-400">
+						Get ideas for your next album cover
+					</div>
 				</div>
 
-				<div className="flex flex-col w-full h-[400px] md:h-[500px] p-2 bg-gray-800 border border-gray-600 rounded mt-8 overflow-y-scroll relative">
-					Content
-				</div>
+				<Form className="relative mt-8 flex w-full flex-col overflow-y-scroll rounded border border-gray-600 bg-gray-800 p-2">
+					<input
+						type="text"
+						name="artistName"
+						id="artistName"
+						placeholder="Your artist name"
+					/>
+				</Form>
 
-				<div className="flex flex-col space-y-2 mt-8 text-sm text-gray-400 text-center">
+				<div className="mt-8 flex flex-col space-y-2 text-center text-sm text-gray-400">
 					Footer
 				</div>
 			</div>
